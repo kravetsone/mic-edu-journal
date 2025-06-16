727589feb6682614cb2a2d7ccb2592bfa2a943e1
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-06-16 21:15:34 +0300

    chore: login and auth

---
+++ apps/backend/src/routes/auth/index.ts
@@ -0,0 +1,47 @@
+import { eq } from "drizzle-orm";
+import { Elysia, t } from "elysia";
+import { db } from "../../db";
+import { usersTable } from "../../db/schema";
+import { authPlugin } from "../../services/auth";
+
+export const authRoutes = new Elysia({
+	prefix: "/auth",
+	tags: ["auth"],
+})
+	.use(authPlugin)
+	.post(
+		"/login",
+		async ({ body: { email, password }, status, jwt }) => {
+			const [user] = await db
+				.select()
+				.from(usersTable)
+				.where(eq(usersTable.email, email));
+			if (!user) return status(401, "UNAUTHORIZED");
+
+			const isPasswordValid = await Bun.password.verify(
+				password,
+				user.password,
+			);
+			if (!isPasswordValid) return status(401, "UNAUTHORIZED");
+
+			const token = await jwt.sign({
+				id: user.id,
+			});
+
+			return {
+				token,
+			};
+		},
+		{
+			body: t.Object({
+				email: t.String(),
+				password: t.String(),
+			}),
+			response: {
+				200: t.Object({
+					token: t.String(),
+				}),
+				401: t.Literal("UNAUTHORIZED"),
+			},
+		},
+	);
---
+++ apps/backend/src/services/auth.ts
@@ -0,0 +1,40 @@
+import { bearer } from "@elysiajs/bearer";
+import jwt from "@elysiajs/jwt";
+import { Elysia, t } from "elysia";
+import { config } from "../config";
+
+export const authPlugin = new Elysia()
+	.use(bearer())
+	.use(
+		jwt({
+			secret: config.JWT_SECRET,
+			schema: t.Object({
+				id: t.String(),
+			}),
+		}),
+	)
+	.macro({
+		auth: {
+			guard: {
+				schema: "standalone",
+				headers: t.Object({
+					Authorization: t.String(),
+				}),
+				response: {
+					401: t.Literal("UNAUTHORIZED"),
+				},
+			},
+			resolve: async ({ jwt, bearer, status }) => {
+				const token = bearer?.token;
+				if (!token) return status(401, "UNAUTHORIZED");
+
+				const payload = await jwt.verify(token);
+
+				if (!payload) return status(401, "UNAUTHORIZED");
+
+				return {
+					userId: payload.id,
+				};
+			},
+		},
+	});
