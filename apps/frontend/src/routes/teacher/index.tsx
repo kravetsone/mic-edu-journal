658cacc28127715b79e355aea36f52c078811278
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-06-17 01:57:34 +0300

    chore: start

--- apps/frontend/src/routes/teacher/index.tsx
+++ apps/frontend/src/routes/teacher/index.tsx
@@ -1,16 +1,20 @@
 import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import dayjs from "dayjs";
+import { useState } from "react";
 
 export const Route = createFileRoute("/teacher/")({
 	component: App,
 });
 
 function App() {
-    const { data } = useQuery({
-        queryKey: ["teacher-schedule"],
-        queryFn: () => api.teacher.schedule.get("/week/1"),
-    });
+	const [weekId, setWeekId] = useState(dayjs().week);
+
+	const { data } = useQuery({
+		queryKey: ["teacher-schedule"],
+		queryFn: () => api.teacher.schedule.week({ weekId: 1 }).get(),
+	});
 
 	return (
 		<div className="w-full h-screen bg-black flex justify-around items-center text-white"></div>
