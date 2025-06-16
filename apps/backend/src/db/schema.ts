d094b244adfbc69288ba445cd0db22209be70ccc
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-06-16 21:23:23 +0300

    chore: students, teachers

--- apps/backend/src/db/schema.ts
+++ apps/backend/src/db/schema.ts
@@ -1,4 +1,6 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const userRoleEnum = pgEnum("user_role", ["student", "teacher"]);
 
 export const usersTable = pgTable("users", {
 	id: uuid("id").primaryKey().defaultRandom(),
@@ -7,8 +9,28 @@ export const usersTable = pgTable("users", {
 	lastName: text("last_name").notNull(),
 	patronymic: text("patronymic"),
 
-	email: text("email").notNull(),
+	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
+
+	role: userRoleEnum("role").notNull(),
+
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const studentsTable = pgTable("students", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	userId: uuid("user_id")
+		.notNull()
+		.references(() => usersTable.id, { onDelete: "cascade" }),
+
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const teachersTable = pgTable("teachers", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	userId: uuid("user_id")
+		.notNull()
+		.references(() => usersTable.id, { onDelete: "cascade" }),
 
 	createdAt: timestamp("created_at").notNull().defaultNow(),
 });
