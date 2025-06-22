6d8edc46a226f8ad05f21af8d6e87953cdd2098a
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-06-23 00:39:42 +0300

    chore: include env

--- apps/frontend/src/lib/api/index.ts
+++ apps/frontend/src/lib/api/index.ts
@@ -1,9 +1,14 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../../../../backend/src/server.ts";
 
-export const api = treaty<App>("http://localhost:3000", {
+const apiUrl = import.meta.env.VITE_API_URL;
+
+if (!apiUrl) {
+	throw new Error("VITE_API_URL is not set");
+}
+
+export const api = treaty<App>(apiUrl, {
 	headers: () => ({
 		Authorization: `Bearer ${localStorage.getItem("token")}`,
 	}),
 });
-
